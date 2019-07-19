6da5527c1dec051b192ea5855e3d5037e1aef1ec
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2019-07-19 14:34:43 -0500

    Update 01_GWAS_plots.R

--- 01_GWAS_plots.R
+++ 01_GWAS_plots.R
@@ -1,9 +1,24 @@
-GWAS_cis <- fread("/home/ashley/LCL_chemotherapy/CEU/CEU_GWAS_results/CEU_GWAS_cisplatin.assoc.txt")
-png(filename = "CEU_GWAS_cisplatin.qqplot.png", res=100)
-qq(GWAS_cis$p_wald)
-dev.off()
-png(filename = "CEU_GWAS_cisplatin.manplot.png", res=100)
-manhattan(GWAS_cis, chr = "chr", bp = "ps", p = "p_wald")
-dev.off()
+#Import necessary library
+library(data.table)
 
+#Create function to paste in drug name
+"%&%" = function(a,b) paste(a,b,sep="")
 
+#Create color vector
+colors<-c("#7514AA", "#E61049", "#FEAB12", "#6FE210")
+
+#Create list of drugs for file input
+drug_list <- c("arac", "capecitabine", "carboplatin", "cisplatin", "daunorubicin", "etoposide", "paclitaxel", "pemetrexed")
+
+#Make QQ and Manhattan plots using loop
+for(drug in drug_list){
+  GWAS_CEU <- fread("/home/ashley/LCL_chemotherapy/CEU/CEU_GWAS_results/CEU_GWAS_" %&% drug %&% ".assoc.txt")
+  
+  png(filename = "CEU_GWAS_" %&% drug %&% ".qqplot.png", res=100)
+  qq(GWAS_CEU$p_wald)
+  dev.off()
+  png(filename = "CEU_GWAS_" %&% drug %&% ".manplot.png", res=100)
+  manhattan(GWAS_CEU, chr = "chr", bp = "ps", p = "p_wald", col = colors)
+  dev.off()
+
+}
