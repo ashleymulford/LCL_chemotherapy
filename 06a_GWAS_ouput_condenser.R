91c750290046fd2b3314419b3acc49c48ceca495
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2019-07-19 13:19:26 -0500

    Update 06a_GWAS_ouput_condenser.R

--- 06a_GWAS_ouput_condenser.R
+++ 06a_GWAS_ouput_condenser.R
@@ -1,10 +1,18 @@
+#Import necessasry libraries
 library(data.table)
 library(tibble)
 
+#Create function to paste in tissue name
 "%&%" = function(a,b) paste(a,b,sep="")
 
+#Create list of drugs for file input
 drug_list <- c("arac", "capecitabine", "carboplatin", "cisplatin", "daunorubicin", "etoposide", "paclitaxel", "pemetrexed")
 
+#Make a data frame with significant results
+  #Read in file
+  #Add column containing drug name
+  #Subset for significance, threshold = 10^-5
+  #Compile significant subsets into single data frame
 for(drug in drug_list){
   YRI_output <- fread("/home/ashley/LCL_chemotherapy/YRI/YRI_GWAS_results/YRI_GWAS_" %&% drug %&% ".assoc.txt")
 
@@ -22,8 +30,10 @@ for(drug in drug_list){
   
 }
 
+#Subset again for significance, threshold = 10^-7
 YRI_GWAS_mostsig <- subset(YRI_GWAS_sign, p_wald <= 10^-7)
 
+#Output data frames into directory
 fwrite(YRI_GWAS_sign, "/home/ashley/LCL_chemotherapy/YRI/YRI_GWAS_results/YRI_GWAS_sig", na = "NA", quote = F, sep = "\t", col.names = T) 
 
 fwrite(YRI_GWAS_mostsig, "/home/ashley/LCL_chemotherapy/YRI/YRI_GWAS_results/YRI_GWAS_mostsig", na = "NA", quote = F, sep = "\t", col.names = T) 
