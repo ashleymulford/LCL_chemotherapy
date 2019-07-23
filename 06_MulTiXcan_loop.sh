49c5f3cf8d5ec2d1a3a002c36b64de93dc1cf758
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2019-07-23 12:38:44 -0500

    Update 06_MulTiXcan_loop.sh

--- 06_MulTiXcan_loop.sh
+++ 06_MulTiXcan_loop.sh
@@ -2,14 +2,11 @@
 declare -a drugs=("arac" "capecitabine" "carboplatin" "cisplatin" "daunorubicin" "etoposide" "paclitaxel" "pemetrexed")
 declare -a pops=("CEU" "YRI")
 
+#Use loop to run MulTiXcan
 for drug in ${drugs[@]}
   do
-  
   for pop in ${pops[@]}
     do
-    
     usr/bin/python MulTiXcan.py --expression_folder /home/ashley/LCL_chemotherapy/${pop}/${pop}_predixcan_predict_output --expression_pattern "${pop}_results_(.*)_predicted_expression.txt" --input_phenos_file /home/ashley/LCL_chemotherapy/${pop}/${pop}_multixcan_phenos/${pop}_${drug}_pheno --input_phenos_column Pheno --mode linear --output /home/ashley/LCL_chemotherapy/${pop}/${pop}_multixcan_output/${pop}_${drug}_multixcan
-    
   done
-
 done
