e8473b5adf431cdc4e43bd4a81561afaeef1bd72
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2019-07-24 11:30:24 -0500

    Update 04_GEMMA_loop.sh

--- 04_GEMMA_loop.sh
+++ 04_GEMMA_loop.sh
@@ -4,12 +4,8 @@ declare -a pops=("CEU" "${pop}")
 
 for drug in ${drugs[@]}
   do
-  
   for pop in ${pops[@]}
     do
-    
     gemma -g /home/ashley/LCL_chemotherapy/${pop}/${pop}.TGP_and_imputed.rmBAD.20130526.geno -p /home/ashley/LCL_chemotherapy/${pop}/${pop}_phenotypes/${pop}_${drug}_bestpheno_noids.txt -a /home/ashley/LCL_chemotherapy/${pop}/${pop}.TGP_and_imputed.rmBAD.20130526.snp.info -k /home/ashley/LCL_chemotherapy/${pop}/${pop}_relationship_matrix_maf.cXX.txt -lmm 4 -o /home/ashley/LCL_chemotherapy/${pop}/${pop}_GWAS_results/${pop}_GWAS_${drug}
-    
   done
-
 done
