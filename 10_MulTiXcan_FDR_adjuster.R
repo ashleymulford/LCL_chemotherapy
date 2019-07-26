62e2af9e160f1717ed80970fa4a0e8e416f80657
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2019-07-26 12:31:44 -0500

    Update 10_MulTiXcan_FDR_adjuster.R

--- 10_MulTiXcan_FDR_adjuster.R
+++ 10_MulTiXcan_FDR_adjuster.R
@@ -4,11 +4,14 @@
 
 drug_list<-c("arac", "cape", "carbo", "cis", "dauno", "etop", "pacl", "peme")
 
+bp_chrome<-fread("/home/ashley/LCL_chemotherapy/BP_Chrome.txt")
+
 for (drug in drug_list) {
   YRI_multixcan<-fread("/home/ashley/LCL_chemotherapy/YRI/YRI_multixcan_output/YRI_" %&% drug %&% "_multixcan")
   pvalues<-select(YRI_multixcan, contains("pvalue"))
   pvalues<-as.vector(unlist(pvalues))
   pvalues_adjusted_fdr<-p.adjust(pvalues, method = "BH")
   YRI_multixcan <- add_column(YRI_multixcan,  pvalues_adjusted_fdr = pvalues_adjusted_fdr , .before = "n_models")
-  fwrite(YRI_multixcan, "/home/ashley/LCL_chemotherapy/YRI/YRI_" %&% drug %&% "_multixcan.txt")
+  YRI_multixcan_wchr <- left_join(YRI_multixcan, bp_chrome, by = c("gene" = "gene"))
+  fwrite(YRI_multixcan_wchr, "/home/ashley/LCL_chemotherapy/YRI/YRI_multixcan_output/YRI_" %&% drug %&% "_multixcan_wchr.txt", col.names = T, row.names = F, sep = "\t", quote = F)
 }
