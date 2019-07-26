855665138caac98682dd1719730a3ebb803784fb
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2019-07-26 12:30:15 -0500

    Update 09d_PrediXcan_FDR_adjuster.R

--- 09d_PrediXcan_FDR_adjuster.R
+++ 09d_PrediXcan_FDR_adjuster.R
@@ -2,13 +2,17 @@
 
 "%&%" = function(a,b) paste(a,b,sep="")
 
-drug_list<-c("arac", "cape", "carbo", "cis", "dauno", "etop", "pacl", "peme")
+bp_chrome <- fread("/home/ashley/LCL_chemotherapy/BP_Chrome.txt")
+
+drug_list <- c("arac", "capecitabine", "carboplatin", "cisplatin", "daunorubicin", "etoposide", "paclitaxel", "pemetrexed")
 
 for (drug in drug_list) {
-  YRI_predixcan<-fread("/home/ashley/LCL_chemotherapy/YRI/YRI_assoc_gemma_output_combined/YRI_assoc_" %&% drug)
-  pvalues<-select(YRI_predixcan, contains("pvalue"))
-  pvalues<-as.vector(unlist(pvalues))
-  pvalues_adjusted_fdr<-p.adjust(pvalues, method = "BH")
-  YRI_predixcan <- add_column(YRI_predixcan,  pvalues_adjusted_fdr = pvalues_adjusted_fdr , .before = "n_models")
-  fwrite(YRI_predixcan, "/home/ashley/LCL_chemotherapy/YRI/YRI_assoc_" %&% drug %&% ".txt")
+  CEU_predixcan <- fread("/home/ashley/LCL_chemotherapy/CEU/CEU_assoc_gemma_output_combined/CEU_assoc_" %&% drug)
+  pvalues <- select(CEU_predixcan, contains("p_wald"))
+  pvalues <- as.vector(unlist(pvalues))
+  pvalues_adjusted_fdr <- p.adjust(pvalues, method = "BH")
+  CEU_predixcan <- add_column(CEU_predixcan,  pvalues_adjusted_fdr = pvalues_adjusted_fdr , .before = "p_lrt")
+  CEU_predixcan <- left_join(CEU_predixcan, bp_chrome, by = c("rs" = "gene"))
+  CEU_predixcan <- select(CEU_predixcan, - chr, - ps)
+  fwrite(CEU_predixcan, "/home/ashley/LCL_chemotherapy/CEU/CEU_assoc_gemma_output_combined/CEU_assoc_adjusted_" %&% drug %&% ".txt")
 }
