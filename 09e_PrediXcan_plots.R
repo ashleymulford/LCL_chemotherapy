c431bfdb7b93e970f9edcaf347d2eaa4a1ec1a1a
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2019-07-26 13:11:58 -0500

    Update 09e_PrediXcan_plots.R

--- 09e_PrediXcan_plots.R
+++ 09e_PrediXcan_plots.R
@@ -13,13 +13,18 @@ colors<-sequential_hcl(4,"SunsetDark")
 drug_list <- c("arac", "capecitabine", "carboplatin", "cisplatin", "daunorubicin", "etoposide", "paclitaxel", "pemetrexed")
 
 #Make QQ and Manhattan plots using loop
+  
 for(drug in drug_list){
   CEU_assoc_adj <- fread("/home/ashley/LCL_chemotherapy/CEU/CEU_assoc_gemma_output_combined/CEU_assoc_adjusted_" %&% drug %&% ".txt")
+  #Remove alleles columns (empty since GEMMA was used for PrediXcan assoc instead of GWAS)
   CEU_assoc_adj<-select(CEU_assoc_adj, 1:3, 6:17)
+  #Remove all rows with NAs
   CEU_assoc_adj<-na.omit(CEU_assoc_adj)
+  
   png(filename = "CEU_assoc_adj_" %&% drug %&% ".qqplot.png", res=100)
   qq(CEU_assoc_adj$p_wald)
   dev.off()
+  
   png(filename = "CEU_assoc_adj_" %&% drug %&% ".manplot.png", res=100)
   manhattan(CEU_assoc_adj, chr = "CHR", bp = "BP", p = "p_wald", col = colors)
   dev.off()
