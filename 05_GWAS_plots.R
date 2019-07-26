7127e82190ebc75e1c2fc3f060eea2b8563972cc
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2019-07-26 13:10:05 -0500

    Update 05_GWAS_plots.R

--- 05_GWAS_plots.R
+++ 05_GWAS_plots.R
@@ -15,10 +15,11 @@ drug_list <- c("arac", "capecitabine", "carboplatin", "cisplatin", "daunorubicin
 #Make QQ and Manhattan plots using loop
 for(drug in drug_list){
   GWAS_CEU <- fread("/home/ashley/LCL_chemotherapy/CEU/CEU_GWAS_results/CEU_GWAS_" %&% drug %&% ".assoc.txt")
-  
+ 
   png(filename = "CEU_GWAS_" %&% drug %&% ".qqplot.png", res=100)
   qq(GWAS_CEU$p_wald)
   dev.off()
+  
   png(filename = "CEU_GWAS_" %&% drug %&% ".manplot.png", res=100)
   manhattan(GWAS_CEU, chr = "chr", bp = "ps", p = "p_wald", col = colors)
   dev.off()
