05b8e4805a6c62224b8660499fbc9586380a1e68
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2019-07-26 13:04:54 -0500

    Update 10a_MulTiXcan_FDR_adjuster.R

--- 10a_MulTiXcan_FDR_adjuster.R
+++ 10a_MulTiXcan_FDR_adjuster.R
@@ -1,11 +1,23 @@
-#adjust pvalues with FDR
+#Import necessary libraries
+library(data.table)
+library(dpylr)
 
+#Create function to paste in drug name
 "%&%" = function(a,b) paste(a,b,sep="")
 
+#Read in table with chr # and bp for genes
+bp_chrome <- fread("/home/ashley/LCL_chemotherapy/BP_Chrome.txt")
+
+#Create list of drugs for file input
 drug_list<-c("arac", "cape", "carbo", "cis", "dauno", "etop", "pacl", "peme")
 
-bp_chrome<-fread("/home/ashley/LCL_chemotherapy/BP_Chrome.txt")
-
+#Make a data frame from MulTiXcan results that includes chr #, bp, and FDR adj p-value
+  #Read in MulTiXcan output file
+  #Create vector of p-values
+  #Adjust p-values with BH (FDR) method
+  #Add new column with adjusted p-values to data frame
+  #Join MulTiXcan data frame with bp_chrome data frame to add columns with chr # and bp
+  #Ouput data frame into directory
 for (drug in drug_list) {
   YRI_multixcan<-fread("/home/ashley/LCL_chemotherapy/YRI/YRI_multixcan_output/YRI_" %&% drug %&% "_multixcan")
   pvalues<-select(YRI_multixcan, contains("pvalue"))
