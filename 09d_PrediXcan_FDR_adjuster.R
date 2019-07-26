f940ed3129082d5508d6cc0693383d81c44350a6
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2019-07-26 13:01:56 -0500

    Update 09d_PrediXcan_FDR_adjuster.R

--- 09d_PrediXcan_FDR_adjuster.R
+++ 09d_PrediXcan_FDR_adjuster.R
@@ -1,11 +1,24 @@
-#adjust pvalues with FDR
+#Import necessary libraries
+library(data.table)
+library(dpylr)
 
+#Create function to paste in drug name
 "%&%" = function(a,b) paste(a,b,sep="")
 
+#Read in table with chr # and bp for genes
 bp_chrome <- fread("/home/ashley/LCL_chemotherapy/BP_Chrome.txt")
 
+#Create list of drugs for file input
 drug_list <- c("arac", "capecitabine", "carboplatin", "cisplatin", "daunorubicin", "etoposide", "paclitaxel", "pemetrexed")
 
+#Make a data frame from combined PrediXcan results that includes chr #, bp, and FDR adj p-value
+  #Read in combined output file
+  #Create vector of p-values
+  #Adjust p-values with BH (FDR) method
+  #Add new column with adjusted p-values to data frame
+  #Join PrediXcan data frame with bp_chrome data frame to add columns with chr # and bp
+  #Remove old/inaccurate chr and bp columns
+  #Ouput data frame into directory
 for (drug in drug_list) {
   CEU_predixcan <- fread("/home/ashley/LCL_chemotherapy/CEU/CEU_assoc_gemma_output_combined/CEU_assoc_" %&% drug)
   pvalues <- select(CEU_predixcan, contains("p_wald"))
