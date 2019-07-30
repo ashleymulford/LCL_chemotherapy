a516d236f102b456dabd58f6d01b9d384c0ad18d
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2019-07-30 14:50:45 -0500

    Update 11_BP_chrome_FUMA.R

--- Scripts/11_BP_chrome_FUMA.R
+++ Scripts/11_BP_chrome_FUMA.R
@@ -1,11 +1,19 @@
+#Import necessary libraries
 library(data.table)
 library(tibble)
 
+#Read in bp_chrome table
 bp_chrome <- fread("/home/ashley/LCL_chemotherapy/BP_Chrome.txt")
+
+#Pull out gene column and create vector
 genes <- select(bp_chrome, 3)
 genes <- as.vector(unlist(genes))
+
+#Remove gene column from data frame
 bp_chrome <- select(bp_chrome, 1,2,4)
 
+#Create list of gene names for FUMA
+  #Create a substring of original gene name, leaving out everything after the decimal
 for (gene in genes) {
   gene<-substr(gene,0,15)
   if(exists("gene_list_fuma")){
@@ -16,5 +24,8 @@ for (gene in genes) {
   }
 }
 
+#Add new gene column to data frame
 bp_chrome_fuma<-add_column(bp_chrome, gene = gene_list_fuma, .before = "CHR")
+
+#Output data frame into directory
 fwrite(bp_chrome_fuma, "/home/ashley/LCL_chemotherapy/BP_Chrome_fuma.txt", col.names = F, quote = F, sep = "\t")
