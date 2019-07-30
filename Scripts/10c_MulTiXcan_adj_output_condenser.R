9ef8595c95cc400bf422646ac98eb7adccb65f01
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2019-07-30 11:42:00 -0500

    Update 10c_MulTiXcan_adj_output_condenser.R

--- Scripts/10c_MulTiXcan_adj_output_condenser.R
+++ Scripts/10c_MulTiXcan_adj_output_condenser.R
@@ -14,26 +14,31 @@ drug_list <- c("arac", "cape", "carbo", "cis", "dauno", "etop", "pacl", "peme")
   #Subset for significance, threshold = .3
   #Compile significant subsets into single data frame
 for(drug in drug_list){
-  CEU_mult <- fread("/home/ashley/LCL_chemotherapy/CEU/CEU_multixcan_output/CEU_" %&% drug %&% "_multixcan_wchr.txt")
-  CEU_mult<-add_column(CEU_mult, drug = drug, .before = "gene")
-  CEU_mult_significant<-subset(CEU_mult, pvalues_adjusted_fdr <= .3)
-  if(exists("CEU_mult_sign3")){
-    CEU_mult_sign3<-merge(x = CEU_mult_sign3, y = CEU_mult_significant, all = TRUE)
+  YRI_mult <- fread("/home/ashley/LCL_chemotherapy/YRI/YRI_multixcan_output/YRI_" %&% drug %&% "_multixcan_wchr.txt")
+  YRI_mult<-add_column(YRI_mult, drug = drug, .before = "pvalue")
+  YRI_mult_significant<-subset(YRI_mult, pvalues_adjusted_fdr <= .3)
+  if(exists("YRI_mult_sign3")){
+    YRI_mult_sign3<-merge(x = YRI_mult_sign3, y = YRI_mult_significant, all = TRUE)
   }
   else{
-    CEU_mult_sign3<-CEU_mult_significant
+    YRI_mult_sign3<-YRI_mult_significant
   }
 }
 
-#Subset again for significance, threshold = .15, .075, and .05
-CEU_mult_sign15 <- subset(CEU_mult_sign3, pvalues_adjusted_fdr <= .15)
-CEU_mult_sign075 <- subset(CEU_mult_sign3, pvalues_adjusted_fdr <= .075)
-CEU_mult_sign05 <- subset(CEU_mult_sign3, pvalues_adjusted_fdr <= .05)
+#Subset again for significance, threshold = .2, .15, .075, and .05
+YRI_mult_sign2 <- subset(YRI_mult_sign3, pvalues_adjusted_fdr <= .2)
+YRI_mult_sign15 <- subset(YRI_mult_sign3, pvalues_adjusted_fdr <= .15)
+YRI_mult_sign075 <- subset(YRI_mult_sign3, pvalues_adjusted_fdr <= .075)
+YRI_mult_sign05 <- subset(YRI_mult_sign3, pvalues_adjusted_fdr <= .05)
 
 #Output data frames into directory
-fwrite(CEU_mult_sign3, "/home/ashley/LCL_chemotherapy/CEU/CEU_multixcan_output/CEU_multixcan_condensed_fdr3", na = "NA", quote = F, sep = "\t", col.names = T) 
-fwrite(CEU_mult_sign15, "/home/ashley/LCL_chemotherapy/CEU/CEU_multixcan_output/CEU_multixcan_condensed_fdr15", na = "NA", quote = F, sep = "\t", col.names = T) 
-fwrite(CEU_mult_sign075, "/home/ashley/LCL_chemotherapy/CEU/CEU_multixcan_output/CEU_multixcan_condensed_fdr075", na = "NA", quote = F, sep = "\t", col.names = T) 
-fwrite(CEU_mult_sign05, "/home/ashley/LCL_chemotherapy/CEU/CEU_multixcan_output/CEU_multixcan_condensed_fdr05", na = "NA", quote = F, sep = "\t", col.names = T) 
+fwrite(YRI_mult_sign3, "/home/ashley/LCL_chemotherapy/YRI/YRI_multixcan_output/YRI_multixcan_condensed_fdr3", na = "NA", quote = F, sep = "\t", col.names = F) 
+fwrite(YRI_mult_sign2, "/home/ashley/LCL_chemotherapy/YRI/YRI_multixcan_output/YRI_multixcan_condensed_fdr2", na = "NA", quote = F, sep = "\t", col.names = F) 
+fwrite(YRI_mult_sign15, "/home/ashley/LCL_chemotherapy/YRI/YRI_multixcan_output/YRI_multixcan_condensed_fdr15", na = "NA", quote = F, sep = "\t", col.names = F) 
+fwrite(YRI_mult_sign075, "/home/ashley/LCL_chemotherapy/YRI/YRI_multixcan_output/YRI_multixcan_condensed_fdr075", na = "NA", quote = F, sep = "\t", col.names = F) 
+fwrite(YRI_mult_sign05, "/home/ashley/LCL_chemotherapy/YRI/YRI_multixcan_output/YRI_multixcan_condensed_fdr05", na = "NA", quote = F, sep = "\t", col.names = F) 
+
+
+
 
 
