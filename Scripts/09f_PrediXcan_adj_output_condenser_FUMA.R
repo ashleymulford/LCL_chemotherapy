f3f501c6d53aa64888b782b6865dcb9440707f2d
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2019-07-31 11:39:58 -0500

    Update 09f_PrediXcan_adj_output_condenser_FUMA.R

--- Scripts/09f_PrediXcan_adj_output_condenser_FUMA.R
+++ Scripts/09f_PrediXcan_adj_output_condenser_FUMA.R
@@ -7,7 +7,7 @@ library(dplyr)
 "%&%" = function(a,b) paste(a,b,sep="")
 
 #Create list of drugs for file input
-drug_list <- c("arac", "capecitabine", "carboplatin", "cisplatin")#, "daunorubicin", "etoposide", "paclitaxel", "pemetrexed")
+drug_list <- c("arac", "capecitabine", "carboplatin", "cisplatin", "daunorubicin", "etoposide", "paclitaxel", "pemetrexed")
 
 #Make a data frame with significant results
   #Read in file
@@ -26,11 +26,15 @@ for(drug in drug_list){
   }
 }
 
+#Pull out gene column and create vector
 genes<-select(YRI_pred_sign6, 3)
 genes<-as.vector(unlist(genes))
 
-YRI_pred_sign6<-select(YRI_pred_sign6, 1:2, 4:16)
+#Remove gene column from data frame
+YRI_pred_sign6<-select(YRI_pred_sign6, 1:2, 4:18)
 
+#Create list of gene names for FUMA
+  #Create a substring of original gene name, leaving out everything after the decimal
 for (gene in genes) {
   gene<-substr(gene,0,15)
   if(exists("gene_list_fuma")){
@@ -41,13 +45,13 @@ for (gene in genes) {
   }
 }
 
+#Add new gene column to data frame
 YRI_pred_sign6<-add_column(YRI_pred_sign6, gene = gene_list_fuma, .before = "drug")
 
 #Subset again for significance, threshold = .3, .2, and .075
 YRI_pred_sign3 <- subset(YRI_pred_sign6, pvalues_adjusted_fdr <= .3)
 YRI_pred_sign2 <- subset(YRI_pred_sign6, pvalues_adjusted_fdr <= .2)
 YRI_pred_sign075 <- subset(YRI_pred_sign6, pvalues_adjusted_fdr <= .075)
-
 
 #Output data frames into directory
 fwrite(YRI_pred_sign6, "/home/ashley/LCL_chemotherapy/YRI/YRI_assoc_gemma_output_combined/YRI_assoc_adj_condensed_fdr6", na = "NA", quote = F, sep = "\t", col.names = T) 
