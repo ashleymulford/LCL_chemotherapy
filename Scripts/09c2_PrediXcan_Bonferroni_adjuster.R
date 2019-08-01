121ce6d0a675b2994e48bd384ee6811fa6746b96
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2019-08-01 15:08:08 -0500

    Create 09c2_PrediXcan_Bonferroni_adjuster.R

---
+++ Scripts/09c2_PrediXcan_Bonferroni_adjuster.R
@@ -0,0 +1,32 @@
+#Import necessary libraries
+library(data.table)
+library(dpylr)
+library(tibble)
+
+#Create function to paste in drug name
+"%&%" = function(a,b) paste(a,b,sep="")
+
+#Read in table with chr # and bp for genes
+bp_chrome <- fread("/home/ashley/LCL_chemotherapy/BP_Chrome.txt")
+
+#Create list of drugs for file input
+drug_list <- c("arac", "capecitabine", "carboplatin", "cisplatin", "daunorubicin", "etoposide", "paclitaxel", "pemetrexed")
+
+#Make a data frame from combined PrediXcan results that includes chr #, bp, and FDR adj p-value
+  #Read in combined output file
+  #Create vector of p-values
+  #Adjust p-values with BH (FDR) method
+  #Add new column with adjusted p-values to data frame
+  #Join PrediXcan data frame with bp_chrome data frame to add columns with chr # and bp
+  #Remove old/inaccurate chr and bp columns
+  #Ouput data frame into directory
+for (drug in drug_list) {
+  YRI_predixcan <- fread("/home/ashley/LCL_chemotherapy/YRI/YRI_assoc_gemma_output_combined/YRI_assoc_" %&% drug)
+  pvalues <- select(YRI_predixcan, contains("p_wald"))
+  pvalues <- as.vector(unlist(pvalues))
+  pvalues_adjusted_fdr <- p.adjust(pvalues, method = "bonferroni")
+  YRI_predixcan <- add_column(YRI_predixcan,  pvalues_adjusted_BF = pvalues_adjusted_fdr , .before = "p_lrt")
+  YRI_predixcan <- left_join(YRI_predixcan, bp_chrome, by = c("rs" = "gene"))
+  YRI_predixcan <- select(YRI_predixcan, - chr, - ps)
+  fwrite(YRI_predixcan, "/home/ashley/LCL_chemotherapy/YRI/YRI_assoc_gemma_output_combined/YRI_assoc_adjusted_bonferroni_" %&% drug %&% ".txt")
+}
