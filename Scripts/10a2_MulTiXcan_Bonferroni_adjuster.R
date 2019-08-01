4bc00c5513e43464d8c32207cd5168db49558aeb
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2019-08-01 15:06:07 -0500

    Create 10a2_MulTiXcan_Bonferroni_adjuster.R

---
+++ Scripts/10a2_MulTiXcan_Bonferroni_adjuster.R
@@ -0,0 +1,30 @@
+#Import necessary libraries
+library(data.table)
+library(dpylr)
+library(tibble)
+
+#Create function to paste in drug name
+"%&%" = function(a,b) paste(a,b,sep="")
+
+#Read in table with chr # and bp for genes
+bp_chrome<-fread("/home/ashley/LCL_chemotherapy/BP_Chrome.txt")
+
+#Create list of drugs for file input
+drug_list<-c("arac", "cape", "carbo", "cis", "dauno", "etop", "pacl", "peme")
+
+#Make a data frame from MulTiXcan results that includes chr #, bp, and FDR adj p-value
+  #Read in MulTiXcan output file
+  #Create vector of p-values
+  #Adjust p-values with Bonferroni method
+  #Add new column with adjusted p-values to data frame
+  #Join MulTiXcan data frame with bp_chrome data frame to add columns with chr # and bp
+  #Ouput data frame into directory
+for (drug in drug_list) {
+  YRI_multixcan<-fread("/home/ashley/LCL_chemotherapy/YRI/YRI_multixcan_output/YRI_" %&% drug %&% "_multixcan")
+  pvalues<-select(YRI_multixcan, contains("pvalue"))
+  pvalues<-as.vector(unlist(pvalues))
+  pvalues_adjusted_fdr<-p.adjust(pvalues, method = "bonferroni")
+  YRI_multixcan <- add_column(YRI_multixcan,  pvalues_adjusted_BF = pvalues_adjusted_fdr , .before = "n_models")
+  YRI_multixcan_wchr <- left_join(YRI_multixcan, bp_chrome, by = c("gene" = "gene"))
+  fwrite(YRI_multixcan_wchr, "/home/ashley/LCL_chemotherapy/YRI/YRI_multixcan_output/YRI_" %&% drug %&% "_multixcan_bonferroni_wchr.txt", col.names = T, row.names = F, sep = "\t", quote = F)
+}
