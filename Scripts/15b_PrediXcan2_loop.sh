35d442b03c7f50cdcc82f64063fe2712e071af32
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2019-08-06 14:05:26 -0500

    Update 15b_PrediXcan2_loop.sh

--- Scripts/15b_PrediXcan2_loop.sh
+++ Scripts/15b_PrediXcan2_loop.sh
@@ -1,8 +1,10 @@
+#Create list of drugs
 declare -a drugs=("arac" "cape" "carbo" "cis" "dauno" "etop" "pacl" "peme")
 
+#Use loop to run PrediXcan step two through GEMMA (accounts for relatedness)
 for db in /home/wheelerlab3/Data/PrediXcan_db/GTEx-V7_HapMap-2017-11-29/*.db
-
   do 
+  #create tissue prefix for output
   prefix=${db#/home/wheelerlab3/Data/PrediXcan_db/GTEx-V7_HapMap-2017-11-29/gtex_v7_}
   prefix=${prefix%_imputed_europeans_tw_0.5_signif.db}
   
