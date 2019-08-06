172507b3477e5b2f250ec7c5865e8409f93e0074
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2019-08-06 14:01:44 -0500

    Update 15a_PrediXcan1_loop.sh

--- Scripts/15a_PrediXcan1_loop.sh
+++ Scripts/15a_PrediXcan1_loop.sh
@@ -1,1 +1,8 @@
+for db in /home/wheelerlab3/Data/PrediXcan_db/GTEx-V7_HapMap-2017-11-29/*.db
 
+  do 
+  prefix=${db#/home/wheelerlab3/Data/PrediXcan_db/GTEx-V7_HapMap-2017-11-29/gtex_v7_}
+  prefix=${prefix%_imputed_europeans_tw_0.5_signif.db}
+  /usr/local/bin/PrediXcan.py --predict --dosages /home/ashley/LCL_chemotherapy/CEU/CEU_predixcan_dosages --dosages_prefix chr --samples /home/ashley/LCL_chemotherapy/CEU/samples.txt --weights $db --output_prefix CEU_test_results_$prefix
+
+done
