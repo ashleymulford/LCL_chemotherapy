6f38366ea6618648f9fe44f17e556db786aa1d3e
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2019-08-07 12:22:37 -0500

    Update 17b_PrediXcan_adj_output_condenser_BF_FUMA_ALL.R

--- Scripts/17b_PrediXcan_adj_output_condenser_BF_FUMA_ALL.R
+++ Scripts/17b_PrediXcan_adj_output_condenser_BF_FUMA_ALL.R
@@ -68,7 +68,6 @@ for (gn in genes_list) {
 #Convert list to single-column data frame
 gn_list_fuma<-as.data.frame(gn_list_fuma)
 
-
 #Output data frames into directory
 fwrite(ALL_pred_sign, "/home/ashley/LCL_chemotherapy/ALL/ALL_assoc_gemma_output_combined/ALL_assoc_adj_bonferroni_FUMA", na = "NA", quote = F, sep = "\t", col.names = T) 
 fwrite(gn_list_fuma, "/home/ashley/LCL_chemotherapy/ALL/ALL_assoc_gemma_output_combined/ALL_predixcan_genes_FUMA", na = "NA", quote = F, sep = "\t", col.names = T)
