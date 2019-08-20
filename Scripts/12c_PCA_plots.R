dced98a5b18b222867f3113838cce65a5848dd1f
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2019-08-20 14:21:50 -0500

    Update 12c_PCA_plots.R

--- Scripts/12c_PCA_plots.R
+++ Scripts/12c_PCA_plots.R
@@ -4,25 +4,21 @@ library(dplyr)
 library(ggplot2)
 
 #Read in fam file and add column names
-popinfo<-fread("/home/ashley/LCL_chemotherapy/ALL/ALL_orderedby_ASN_CEU_YRI.fam") %>% select (V1,V2,V3)
-colnames(popinfo)<-c("pop", "FID", "IID")
+fam<-fread("/home/ashley/LCL_chemotherapy/ALL/ALL_orderedby_ASN_CEU_YRI.fam") %>% select (V1,V2,V3)
+colnames(fam)<-c("pop", "FID", "IID")
 
-#Read in pcs (plink output)
-pcs<-fread("/home/ashley/LCL_chemotherapy/ALL/ALL_plinkfiles_&_PCA/ALL_subset.eigenvec", header=F)
+#Read in pcs (king output)
+pcs <- fread("/home/ashley/LCL_chemotherapy/ALL/ALL_plinkfiles_PCA/kingpc.txt")
 
-#Merge and rename columns with first 10 pcs
-pcdf<-data.frame(popinfo, pcs[,3:ncol(pcs)]) %>% rename(PC1=V3,PC2=V4,PC3=V5,PC4=V6,PC5=V7,PC6=V8,PC7=V9,PC8=V10,PC9=V11,PC10=V12)
+#Join fam and pcs to reorder pcs and add pop 
+pcdf <- left_join(fam, pcs)
 
-#Calcuate proportion variance explained by each PC
-eval<-scan("/home/ashley/LCL_chemotherapy/ALL/ALL_plinkfiles_&_PCA/ALL_subset.eigenval")[1:10]
-skree<-round(eval/sum(eval),3)
-skree<-cbind.data.frame(skree,c(1,2,3,4,5,6,7,8,9,10))
-colnames(skree)<-c("percent_var", "PC")
+#Make covariate file for gemma input
+covariates<-select(pcdf, 8:17)
+fwrite(covariates, "/home/ashley/LCL_chemotherapy/ALL/ALL_covariates_king.txt")
 
 #Make PCA plots:
-pdf("/home/ashley/LCL_chemotherapy/ALL/ALL_plinkfiles_&_PCA/ALL_PCA_plots.pdf")
-
-ggplot(data=skree, aes(x=PC, y=percent_var)) + geom_point() + geom_line() + scale_x_continuous(breaks = 1:10) + ggtitle("Proportion of variance explained")
+pdf("/home/ashley/LCL_chemotherapy/ALL/ALL_plinkfiles_PCA/ALL_PCA_plots_king.pdf")
 
 #PCA Plot 1 (PC1 vs PC2)
 ggplot() + geom_point(data=pcdf,aes(x=PC1,y=PC2,col=pop,shape=pop)) + theme_bw() + scale_colour_brewer(palette="Set1") + ggtitle("PC1 vs PC2")
@@ -34,3 +30,4 @@ ggplot() + geom_point(data=pcdf,aes(x=PC1,y=PC3,col=pop,shape=pop)) + theme_bw()
 ggplot() + geom_point(data=pcdf,aes(x=PC2,y=PC3,col=pop,shape=pop)) + theme_bw() + scale_colour_brewer(palette="Set1") + ggtitle("PC2 vs PC3")
 
 dev.off() 
+
