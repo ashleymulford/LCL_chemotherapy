7ca36b6298b00cbd69bf7bb854c6e2928f597dba
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2019-08-21 14:40:08 -0500

    Update 10a1_MulTiXcan_BH_adjuster.R

--- Scripts/10a1_MulTiXcan_BH_adjuster.R
+++ Scripts/10a1_MulTiXcan_BH_adjuster.R
@@ -23,8 +23,8 @@ for (drug in drug_list) {
   YRI_multixcan<-fread("/home/ashley/LCL_chemotherapy/YRI/YRI_multixcan_output/YRI_" %&% drug %&% "_multixcan")
   pvalues<-select(YRI_multixcan, contains("pvalue"))
   pvalues<-as.vector(unlist(pvalues))
-  pvalues_adjusted_fdr<-p.adjust(pvalues, method = "BH")
-  YRI_multixcan <- add_column(YRI_multixcan,  pvalues_adjusted_fdr = pvalues_adjusted_fdr , .before = "n_models")
+  pvalues_adjusted_BH<-p.adjust(pvalues, method = "BH")
+  YRI_multixcan <- add_column(YRI_multixcan,  pvalues_adjusted_BH = pvalues_adjusted_BH , .before = "n_models")
   YRI_multixcan_wchr <- left_join(YRI_multixcan, bp_chrome, by = c("gene" = "gene"))
   fwrite(YRI_multixcan_wchr, "/home/ashley/LCL_chemotherapy/YRI/YRI_multixcan_output/YRI_multixcan_" %&% drug %&% "_adj_BH.txt", col.names = T, row.names = F, sep = "\t", quote = F)
 }
