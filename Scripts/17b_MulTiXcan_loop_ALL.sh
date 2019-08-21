832cebe894f68488dcad2bc5b198229e3e94586e
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2019-08-21 11:25:33 -0500

    Update 17b_MulTiXcan_loop_ALL.sh

--- Scripts/17b_MulTiXcan_loop_ALL.sh
+++ Scripts/17b_MulTiXcan_loop_ALL.sh
@@ -4,5 +4,5 @@ declare -a drugs=("arac" "cape" "carbo" "cis" "dauno" "etop" "pacl" "peme")
 #Use loop to run MulTiXcan
 for drug in ${drugs[@]}
   do
-  usr/bin/python MulTiXcan.py --expression_folder /home/ashley/LCL_chemotherapy/ALL/ALL_predixcan_predict_output --expression_pattern "ALL_results_(.*)_predicted_expression.txt" --input_phenos_file /home/ashley/LCL_chemotherapy/ALL/ALL_multixcan_phenos/ALL_${drug}.txt --input_phenos_column Pheno --mode linear --output /home/ashley/LCL_chemotherapy/ALL/ALL_multixcan_output/ALL_${drug}_multixcan
+  usr/bin/python MulTiXcan.py --expression_folder /home/ashley/LCL_chemotherapy/ALL/ALL_predixcan_predict_output --expression_pattern "ALL_results_(.*)_predicted_expression.txt" --input_phenos_file /home/ashley/LCL_chemotherapy/ALL/ALL_multixcan_phenos/ALL_${drug}.txt --input_phenos_column Pheno --covariates_file /home/ashley/LCL_chemotherpay/ALL/ALL_covariates.txt --mode linear --output /home/ashley/LCL_chemotherapy/ALL/ALL_multixcan_output/ALL_${drug}_multixcan
 done
