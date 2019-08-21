f6c408209a4768efffccffed3f8494f4ed93896c
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2019-08-21 10:43:07 -0500

    Update 14b_GWAS_GEMMA_ALL_loop.sh

--- Scripts/14b_GWAS_GEMMA_ALL_loop.sh
+++ Scripts/14b_GWAS_GEMMA_ALL_loop.sh
@@ -4,5 +4,5 @@ declare -a drugs=("arac" "cape" "carbo" "cis" "dauno" "etop" "pacl" "peme")
 #Use loop to run GWAS through GEMMA (accounts for relatedness)
 for drug in ${drugs[@]}
   do
-  gemma -g /home/ashley/LCL_chemotherapy/ALL/ALL_orderedby_ASN_CEU_YRI.geno -p /home/ashley/LCL_chemotherapy/ALL/ALL_phenotypes/ALL_${drug}_bestpheno_noids.txt -a /home/ashley/LCL_chemotherapy/ALL/ALL_snp.info -k /home/ashley/LCL_chemotherapy/ALL/ALL_relationship_matrix_maf.cXX.txt -c /home/ashley/LCL_chemotherapy/ALL/ALL_covariates.txt -lmm 4 -o /home/ashley/LCL_chemotherapy/ALL/ALL_GWAS_results/ALL_GWAS_${drug}
+  gemma -g /home/ashley/LCL_chemotherapy/ALL/ALL_orderedby_ASN_CEU_YRI.geno -p /home/ashley/LCL_chemotherapy/ALL/ALL_phenotypes/ALL_${drug}_bestpheno_noids.txt -a /home/ashley/LCL_chemotherapy/ALL/ALL_snp.info -k /home/ashley/LCL_chemotherapy/ALL/ALL_relationship_matrix_maf.cXX.txt -c /home/ashley/LCL_chemotherapy/ALL/ALL_covariates.txt -lmm 4 -o ALL_GWAS_${drug}
 done
