3505c8aed6ddcf21407aede808464eda409b9156
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2019-08-21 14:40:28 -0500

    Update 10a2_MulTiXcan_Bonferroni_adjuster.R

--- Scripts/10a2_MulTiXcan_Bonferroni_adjuster.R
+++ Scripts/10a2_MulTiXcan_Bonferroni_adjuster.R
@@ -23,8 +23,8 @@ for (drug in drug_list) {
   YRI_multixcan<-fread("/home/ashley/LCL_chemotherapy/YRI/YRI_multixcan_output/YRI_" %&% drug %&% "_multixcan")
   pvalues<-select(YRI_multixcan, contains("pvalue"))
   pvalues<-as.vector(unlist(pvalues))
-  pvalues_adjusted_fdr<-p.adjust(pvalues, method = "bonferroni")
-  YRI_multixcan <- add_column(YRI_multixcan,  pvalues_adjusted_BF = pvalues_adjusted_fdr , .before = "n_models")
+  pvalues_adjusted_BF<-p.adjust(pvalues, method = "bonferroni")
+  YRI_multixcan <- add_column(YRI_multixcan,  pvalues_adjusted_BF = pvalues_adjusted_BF , .before = "n_models")
   YRI_multixcan_wchr <- left_join(YRI_multixcan, bp_chrome, by = c("gene" = "gene"))
   fwrite(YRI_multixcan_wchr, "/home/ashley/LCL_chemotherapy/YRI/YRI_multixcan_output/YRI_multixcan_" %&% drug %&% "_adj_bonferroni.txt", col.names = T, row.names = F, sep = "\t", quote = F)
 }
