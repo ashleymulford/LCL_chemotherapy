ff3d0aea7136ede8a78463524328305e5804c347
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2019-08-21 10:51:54 -0500

    Update 16b_PrediXcan2_loop.sh

--- Scripts/16b_PrediXcan2_loop.sh
+++ Scripts/16b_PrediXcan2_loop.sh
@@ -9,6 +9,6 @@ for db in /home/wheelerlab3/Data/PrediXcan_db/GTEx-V7_HapMap-2017-11-29/*.db
   prefix=${prefix%_imputed_europeans_tw_0.5_signif.db}
   for drug in ${drugs[@]}
     do
-    /usr/local/bin/gemma -g /home/ashley/LCL_chemotherapy/ALL/ALL_assoc_gemma_input/$prefix -notsnp -p /home/ashley/LCL_chemotherapy/ALL/ALL_phenotypes/ALL_${drug}_bestpheno_noids.txt -k ALL_relationship_matrix_maf.cXX.txt -lmm 4 -o ALL_assoc_${drug}_$prefix
+    /usr/local/bin/gemma -g /home/ashley/LCL_chemotherapy/ALL/ALL_assoc_gemma_input/$prefix -notsnp -p /home/ashley/LCL_chemotherapy/ALL/ALL_phenotypes/ALL_${drug}_bestpheno_noids.txt -k ALL_relationship_matrix_maf.cXX.txt -c ALL_covariates.txt -lmm 4 -o ALL_assoc_${drug}_$prefix
   done
 done
