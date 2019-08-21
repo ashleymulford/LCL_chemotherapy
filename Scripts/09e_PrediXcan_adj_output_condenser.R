ea87bec806b75b98ccd4e4b5c41a07ed41cbf780
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2019-08-21 14:25:48 -0500

    Update 09e_PrediXcan_adj_output_condenser.R

--- Scripts/09e_PrediXcan_adj_output_condenser.R
+++ Scripts/09e_PrediXcan_adj_output_condenser.R
@@ -14,7 +14,7 @@ drug_list <- c("arac", "cape", "carbo", "cis", "dauno", "etop", "pacl", "peme")
   #Subset for significance, threshold = .3
   #Compile significant subsets into single data frame
 for(drug in drug_list){
-  YRI_pred <- fread("/home/ashley/LCL_chemotherapy/YRI/YRI_assoc_gemma_output_combined/YRI_assoc_adjusted_" %&% drug %&% ".txt")
+  YRI_pred <- fread("/home/ashley/LCL_chemotherapy/YRI/YRI_assoc_gemma_output_combined/YRI_predixcan_adjusted_" %&% drug %&% ".txt")
   YRI_pred<-add_column(YRI_pred, drug = drug, .before = "tissue")
   YRI_pred_significant<-subset(YRI_pred, pvalues_adjusted_fdr <= .3)
   if(exists("YRI_pred_sign3")){
@@ -30,9 +30,9 @@ YRI_pred_sign2 <- subset(YRI_pred_sign3, pvalues_adjusted_fdr <= .2)
 YRI_pred_sign075 <- subset(YRI_pred_sign3, pvalues_adjusted_fdr <= .075)
 
 #Output data frames into directory
-fwrite(YRI_pred_sign3, "/home/ashley/LCL_chemotherapy/YRI/YRI_assoc_gemma_output_combined/YRI_assoc_adj_condensed_fdr3", na = "NA", quote = F, sep = "\t", col.names = T) 
-fwrite(YRI_pred_sign2, "/home/ashley/LCL_chemotherapy/YRI/YRI_assoc_gemma_output_combined/YRI_assoc_adj_condensed_fdr2", na = "NA", quote = F, sep = "\t", col.names = T) 
-fwrite(YRI_pred_sign075, "/home/ashley/LCL_chemotherapy/YRI/YRI_assoc_gemma_output_combined/YRI_assoc_adj_condensed_fdr075", na = "NA", quote = F, sep = "\t", col.names = T) 
+fwrite(YRI_pred_sign3, "/home/ashley/LCL_chemotherapy/YRI/YRI_assoc_gemma_output_combined/YRI_pred_adj_condensed_fdr3", na = "NA", quote = F, sep = "\t", col.names = T) 
+fwrite(YRI_pred_sign2, "/home/ashley/LCL_chemotherapy/YRI/YRI_assoc_gemma_output_combined/YRI_pred_adj_condensed_fdr2", na = "NA", quote = F, sep = "\t", col.names = T) 
+fwrite(YRI_pred_sign075, "/home/ashley/LCL_chemotherapy/YRI/YRI_assoc_gemma_output_combined/YRI_pred_adj_condensed_fdr075", na = "NA", quote = F, sep = "\t", col.names = T) 
 
 
 
